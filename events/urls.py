f9772c0d5d825eaf24cf87b88e8b12bcca9adcdd
Laurent Peuch <cortex@worlddomination.be>
2014-12-06 03:22:26 +0100

    [mod] finally, template view for home in wanwana.urls and namespaces events related urls

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('events.views',
-    url(r'^$', TemplateView.as_view(template_name="home.haml"), name='home'),
 )
--- wanawana/urls.py
+++ wanawana/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('events.urls')),
+    url(r'^$', TemplateView.as_view(template_name="home.haml"), name='home'),
+    url(r'^events/', include('events.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
