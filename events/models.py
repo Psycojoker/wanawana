a541b33e1d4ee0b331d7c92138b428c69df5c60f
Laurent Peuch <cortex@worlddomination.be>
2014-12-07 20:34:49 +0100

    [fix] location should be nullable

--- events/models.py
+++ events/models.py
@@ -14,7 +14,7 @@ class Event(models.Model):
     date = models.DateField(null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
 
-    location_address = models.CharField(max_length=255)
+    location_address = models.CharField(max_length=255, null=True, blank=True)
 
     # location_gps_lat = models.FloatField(null=True, blank=True)
     # location_gps_lon = models.FloatField(null=True, blank=True)
