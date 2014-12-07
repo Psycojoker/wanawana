b91766898176b82315fc2a765031838c0f40b2e2
Laurent Peuch <cortex@worlddomination.be>
2014-12-07 16:59:37 +0100

    [enh] add a model to store events

--- events/models.py
+++ events/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_modification_date = models.DateTimeField(auto_now_add=True)
+
+    admin_id = models.EmailField(null=True, blank=True)
+
+    date = models.DateField()
+    time = models.TimeField()
+
+    location_address = models.CharField(max_length=255)
+
+    location_gps_lat = models.FloatField()
+    location_gps_lon = models.FloatField()
