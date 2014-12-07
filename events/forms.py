54cdb4cad2783d2c68f6d2f4100cab9254dba15b
Laurent Peuch <cortex@worlddomination.be>
2014-12-07 20:05:15 +0100

    [enh] basic event creation

--- events/forms.py
+++ events/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+from .utils import generate_random_password
+from .models import Event
+
 
 class NewEventForm(forms.Form):
     title = forms.CharField()
@@ -9,6 +12,14 @@ class NewEventForm(forms.Form):
     date = forms.DateField(required=False)
     time = forms.TimeField(required=False)
     location_address = forms.CharField(required=False)
+
+    def generate_admin_id(self):
+        password = generate_random_password(15)
+
+        while Event.objects.filter(admin_id=password):
+            password = generate_random_password(15)
+
+        return password
 
     # wait for django leaflet
     # location_gps_lat
--- events/views.py
+++ events/views.py
@@ -1,10 +1,31 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 
 from .forms import NewEventForm
+from .models import Event
 
 
 def new_event(request):
-    form = NewEventForm()
+    form = NewEventForm(request.POST) if request.method == "POST" else NewEventForm()
+
+    if request.method == "POST" and form.is_valid():
+        event = Event()
+        event.title = form.cleaned_data["title"]
+        # event.slug = form.get_slug()
+
+        event.description = form.cleaned_data["description"]
+
+        event.admin_id = form.generate_admin_id()
+
+        event.date = form.cleaned_data["date"]
+        event.time = form.cleaned_data["time"]
+
+        event.location_address = form.cleaned_data["location_address"]
+
+        event.save()
+
+        return HttpResponseRedirect(reverse("event_admin", args=(event.admin_id,)))
 
     return render(request, "events/new.haml", {
         "form": form,
