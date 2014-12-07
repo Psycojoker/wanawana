968565134d5397cd5ecc6241c1f798b8f680e65b
Laurent Peuch <cortex@worlddomination.be>
2014-12-07 16:59:31 +0100

    [enh] add more fields to form

--- events/forms.py
+++ events/forms.py
@@ -5,3 +5,10 @@ class NewEventForm(forms.Form):
     title = forms.CharField()
     description = forms.CharField()
     admin_email = forms.EmailField(required=False)
+    date = forms.DateField(required=False)
+    time = forms.TimeField(required=False)
+    location_address = forms.CharField()
+
+    # wait for django leaflet
+    # location_gps_lat
+    # location_gps_lon
