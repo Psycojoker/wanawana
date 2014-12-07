17d38c587bcafee443f3f8c42b5048fdc7fd032d
Laurent Peuch <cortex@worlddomination.be>
2014-12-07 17:59:48 +0100

    [mod] finally, I only need a title for the event

--- events/forms.py
+++ events/forms.py
@@ -3,12 +3,12 @@ from django import forms
 
 class NewEventForm(forms.Form):
     title = forms.CharField()
-    slug = forms.SlugField()
-    description = forms.CharField()
+    slug = forms.SlugField(required=False)
+    description = forms.CharField(required=False)
     admin_email = forms.EmailField(required=False)
     date = forms.DateField(required=False)
     time = forms.TimeField(required=False)
-    location_address = forms.CharField()
+    location_address = forms.CharField(required=False)
 
     # wait for django leaflet
     # location_gps_lat
