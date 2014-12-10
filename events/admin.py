e9ed7d3a602b3a41e67d5d0b955dbce049736e49
Laurent Peuch <cortex@worlddomination.be>
2014-12-10 05:45:32 +0100

    [enh] add events to admin, yay

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Event, EventAdmin)
