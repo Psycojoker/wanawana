22666b57d025a4202a409edafcc072b653922108
Laurent Peuch <cortex@worlddomination.be>
2014-12-21 09:47:42 +0100

    [fix] uuid also include '-'

--- events/urls.py
+++ events/urls.py
@@ -7,6 +7,6 @@ from .models import Event
 
 urlpatterns = patterns('events.views',
     url(r'^new/$', 'new_event', name='new_event'),
-    url(r'^admin/(?P<admin_id>[a-zA-Z0-9]+)/$', EventAdminView.as_view(), name='event_admin'),
+    url(r'^admin/(?P<admin_id>[a-zA-Z0-9-]+)/$', EventAdminView.as_view(), name='event_admin'),
     url(r'^(?P<slug>[a-zA-Z0-9-]+)/$', DetailView.as_view(model=Event, template_name="events/event_detail.haml"), name='event_detail'),
 )
