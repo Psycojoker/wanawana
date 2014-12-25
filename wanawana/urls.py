a107c1eb8c8830d2f2054756133d1775c1b1022e
Laurent Peuch <cortex@worlddomination.be>
2014-12-25 09:00:35 +0100

    [mod] don't prefix events urls with 'events/'

--- wanawana/urls.py
+++ wanawana/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="home.haml"), name='home'),
-    url(r'^events/', include('events.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('events.urls')),
 )
