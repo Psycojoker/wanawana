a7a8e266c7497bfe9ab3a3bba0346e8c5e595ac7
Laurent Peuch <cortex@worlddomination.be>
2014-12-30 11:35:55 +0100

    [fix] _ is a valid slug caracter

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('events.views',
     url(r'^new/$', 'new_event', name='new_event'),
     url(r'^event_admin/(?P<admin_id>[a-zA-Z0-9-]+)/$', 'event_admin', name='event_admin'),
-    url(r'^(?P<slug>[a-zA-Z0-9-]+)/(?P<user_uuid>[a-zA-Z0-9-]+)/$', 'event_view', name='event_detail_uuid'),
-    url(r'^(?P<slug>[a-zA-Z0-9-]+)/$', 'event_view', name='event_detail'),
+    url(r'^(?P<slug>[a-zA-Z0-9-_]+)/(?P<user_uuid>[a-zA-Z0-9-]+)/$', 'event_view', name='event_detail_uuid'),
+    url(r'^(?P<slug>[a-zA-Z0-9-_]+)/$', 'event_view', name='event_detail'),
 )
