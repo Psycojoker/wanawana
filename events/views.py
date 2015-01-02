9ce218d5c667309d892a27d993cf28d99e2a9fdb
Laurent Peuch <cortex@worlddomination.be>
2015-01-02 16:59:38 +0100

    [enh] save send_notifications email preference in event admin

--- events/views.py
+++ events/views.py
@@ -83,6 +83,7 @@ def event_admin(request, admin_id):
         event.description = form.cleaned_data["description"]
 
         event.admin_email = form.cleaned_data["admin_email"]
+        event.send_notification_emails = form.cleaned_data["send_notification_emails"]
 
         event.date = form.cleaned_data["date"]
         event.time = form.cleaned_data["time"]
